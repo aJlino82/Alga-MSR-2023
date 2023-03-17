ed41d8769a80211f335d9b5e7431b0435fcec9d7
Adailton Junior <adailton_jr@hotmail.com>
2023-03-17 16:59:39 -0300

    modificações

--- src/main/java/com/sms/loginapp/controllers/MvcUserController.java
+++ src/main/java/com/sms/loginapp/controllers/MvcUserController.java
@@ -3,10 +3,12 @@ package com.sms.loginapp.controllers;
 
 import com.sms.loginapp.models.User;
 import com.sms.loginapp.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
@@ -16,14 +18,31 @@ public class MvcUserController {
     @Autowired
     UserService userService;
 
-    public ModelAndView getIndex(Model model) {
+    @GetMapping
+    public ModelAndView getIndex() {
         ModelAndView mv = new ModelAndView("index");
-        User user = new User();
-        model.addAttribute(user);
         return mv;
     }
 
+    @GetMapping("/addView")
+    public ModelAndView getCadastro(Model model) {
+        ModelAndView mv = new ModelAndView("cadastro");
+        User user = new User();
+        model.addAttribute("user", user);
+        return mv;
+    }
 
-    public
+    @PostMapping("/addUser")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ModelAndView addUser(@Valid User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            ModelAndView mv = new ModelAndView("cadastro");
+            System.out.println("Erro ao criar usuário.");
+        }
+        ModelAndView mv = new ModelAndView("index");
+        userService.save(user);
+        return mv;
+    }
 
 }
